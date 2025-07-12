42fc03e6cd9d812672bef2b21e0de6d3302fa536
WiseyXD <aryan.s.nag@gmail.com>
2025-07-12 21:33:08 +0530

    fix : drizzle setup complete with api calls setup

--- worker/index.ts
+++ worker/index.ts
@@ -1,19 +1,18 @@
+import { drizzle } from "drizzle-orm/d1";
+import { users } from "../drizzle/schema";
 import { Hono } from "hono";
 
 export type Env = {
   DB: D1Database;
-  DB_ID: string;
-  ACCOUNT_ID: string;
-  D1_TOKEN: string;
-  MY_VAR: string;
 };
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) =>
-  c.json({
-    name: `${c.env.MY_VAR}`,
-  }),
-);
+app.get("/api/", async (c) => {
+  const db = drizzle(c.env.DB);
+  const results = await db.select().from(users).all();
+  console.log(results);
+  return c.json("Hello World!");
+});
 
 export default app;
